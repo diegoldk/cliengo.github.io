217fcb6304423adf5c004f96bda557a4370594bc
Mariano Yepes <marianoyepes@gmail.com>
2017-05-04 18:57:38 -0300

    Create instapage.js

---
+++ scripts/instapage.js
@@ -0,0 +1,20 @@
+/** código para guardar el gclid de adwords en el campo hidden "gclid" de instapage, y que lo pueda enviar luego a salesforce */
+
+function getParam(p){
+	var match = RegExp('[?&]' + p + '=([^&]*)').exec(window.location.search);
+	return match && decodeURIComponent(match[1].replace(/\+/g, ' '));
+}
+
+var gclid = getParam('gclid');
+
+if(gclid){
+	/* Este es el selector de instapage para el nombre 'gclid' porque lo encodean en base 64  */
+	var inputs = ijQuery('input[name="Z2NsaWQ="]'); 
+	if(inputs == null){
+		console.error('Leadaki: Hay gclid %s pero no inputs hidden de name "gclid"', gclid);
+	}
+	inputs.val(gclid);
+	console.log('Leadaki: guardado el gclid %s correctamente en %s campos hidden', gclid, inputs.size())
+} else {
+	console.log('Leadaki: no se encontró ningún gclid');
+}
